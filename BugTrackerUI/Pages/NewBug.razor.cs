0e21b5c6c87142f9c2d621668cddcc3295449deb
Menno Visser <Menno.Visser@solera.nl>
2021-04-22 11:25:51 +0200

    Working with Services and Data

--- BugTrackerUI/Pages/NewBug.razor.cs
+++ BugTrackerUI/Pages/NewBug.razor.cs
@@ -8,18 +8,6 @@ namespace BugTrackerUI.Pages
 {
     public partial class NewBug
     {
-        public Bug AddBug { get; set; } = new Bug();
-
-        [Inject]
-        public IBugService BugService { get; set; }
-
-        [Inject]
-        public NavigationManager NavService { get; set; }
-
-        protected void HandleValidSubmit()
-        {
-            BugService.AddBug(AddBug);
-            NavService.NavigateTo("");
-        }
+        
     }
 }
