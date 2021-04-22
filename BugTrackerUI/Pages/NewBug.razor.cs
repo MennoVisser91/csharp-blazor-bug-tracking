a5cb0715248cd44bbe5b873f39e5b7b64b92d617
Menno Visser <Menno.Visser@solera.nl>
2021-04-22 11:22:59 +0200

    Working with Services and Data

--- BugTrackerUI/Pages/NewBug.razor.cs
+++ BugTrackerUI/Pages/NewBug.razor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,5 +9,17 @@ namespace BugTrackerUI.Pages
     public partial class NewBug
     {
         public Bug AddBug { get; set; } = new Bug();
+
+        [Inject]
+        public IBugService BugService { get; set; }
+
+        [Inject]
+        public NavigationManager NavService { get; set; }
+
+        protected void HandleValidSubmit()
+        {
+            BugService.AddBug(AddBug);
+            NavService.NavigateTo("");
+        }
     }
 }
